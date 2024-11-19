b0947a654b24f1b001d4bc130c2a6a0cc3a8a8fc
fchristian1 <fchristian1@gmx.net>
2024-11-19 12:24:29 +0100

    add install grafana

--- 3-view-data/grafana/install.sh
+++ 3-view-data/grafana/install.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
+# https://github.com/grafana/grafana.git
+tags=($("../../common/getTagsFromRepository.sh" https://github.com/grafana/grafana.git))
+index=$((${#tags[@]} - 1))
+echo $1
+
+# execute the menuSelectOnThree.sh script with two parameters tags and index to return the selected version
+
+version=$(
+    (
+        ../../common/menuSelectOnThree.sh $index "${tags[@]}"
+    ) | tee /dev/null
+)
+
 # check is ubuntu or cent os
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$ID
 fi
 
+version=$(echo $version | sed 's/-/~/g')
 if [ "$OS" == "ubuntu" ]; then
     echo "Installing Grafana on Ubuntu"
+    # $version replace - to ~
+    sudo apt-get install -y adduser libfontconfig1 musl &&
+        wget https://dl.grafana.com/oss/release/grafana_"$version"_amd64.deb &&
+        sudo dpkg -i grafana_11.3.0+security~01_amd64.deb &&
+        sudo systemctl enable grafana-server &&
+        sudo systemctl start grafana-server
+
 elif [ "$OS" == "centos" ]; then
     echo "Installing Grafana on CentOS"
+    sudo yum install -y https://dl.grafana.com/oss/release/grafana-"$version"-1.x86_64.rpm &&
+        sudo systemctl enable grafana-server &&
+        sudo systemctl start grafana-server
 else
     echo "OS not supported"
     exit 1
